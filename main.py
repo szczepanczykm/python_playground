298d875b727e7f5b13f233024ca5e1b83536e15b
Mateusz Szczepanczyk <mateusz.szczepanczyk@payability.com>
2021-03-14 22:44:03 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import glob
+import json
+import os
+import string
+import subprocess
+import random
+from datetime import datetime, timedelta, timezone
+
+import boto3
+import logging
+
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
